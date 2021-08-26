f77947ac9951c9ef9203d2bfc8ddaeb2372dab01
Mastura <88199179+Mohenee@users.noreply.github.com>
2021-08-26 21:56:25 +0600

    assignment-1

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//Four types of data
+var intVariable =67;
+console.log(intVariable);
+
+var floatVariable =14.07;
+console.log(floatVariable)
+
+var itIsLie = false;
+console.log(itIsLie)
+
+var strVariable = " 123 ";
+console.log(strVariable)
+
+//Parse the string and add
+
+    var strVariable = " 123 ";
+    var strVariable2 = parseInt(strVariable) ;
+
+    strVariable2+=intVariable;
+    console.log(strVariable2);
+
+//Random no generate
+
+console.log(Math.floor(Math.random()*(20-10))+10);
+
